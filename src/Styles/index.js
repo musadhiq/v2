5b2cc393fa7b2618f87cc8b73cf14187b821cd93
musadhiq <musadhiqmp@gmail.com>
2022-07-15 16:40:47 +0530

    initialized

---
+++ src/Styles/index.js
@@ -0,0 +1,3 @@
+export const bg = "#161B22";
+export const green = "#64FFDA";
+export const text = "#DCDDE4";
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import { MainHeader } from "./Styles";
+function Header() {
+  return <MainHeader>hehe</MainHeader>;
+}
+
+export default Header;
